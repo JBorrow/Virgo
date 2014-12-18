d14af419cfa7f7b93a4305ac063379ae2ec9445b
JBorrow <joshua.borrow@durham.ac.uk>
2014-12-18 15:55:48 +0000

    added sticky footer

--- Web/parts/footer.php
+++ Web/parts/footer.php
@@ -1,7 +1,13 @@
+</div>
+
+<footer class='footer'>
+	<div class='container'>
+		<p>This website is currently under construction, please come back later</p>
+	</div>
+</footer>
+
 <!-- Bootstrap core JavaScript
 <================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
-
-</div>
